8dd2f6a809fa7dc97d837e951ae3d163976a6d93
adarshsingh196999-max <adarshsingh196999@gmail.com>
2026-09-08 14:35:20 +0530

    chore: upgrade Qontri to Expo SDK 57

--- artifacts/mobile/metro.config.js
+++ artifacts/mobile/metro.config.js
@@ -1,37 +1,5 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
-config.watchFolders = (config.watchFolders || []).filter(
-  (folder) => !/@clerk.*_tmp_/.test(folder),
-);
-
-const originalBlockList = config.resolver?.blockList;
-const clerkTmpPattern = /@clerk.*_tmp_/;
-
-if (Array.isArray(originalBlockList)) {
-  config.resolver = {
-    ...config.resolver,
-    blockList: [...originalBlockList, clerkTmpPattern],
-  };
-} else if (originalBlockList) {
-  config.resolver = {
-    ...config.resolver,
-    blockList: [originalBlockList, clerkTmpPattern],
-  };
-} else {
-  config.resolver = {
-    ...config.resolver,
-    blockList: clerkTmpPattern,
-  };
-}
-
-module.exports = config;
-// Ensure package exports resolution is disabled for metro in monorepos
-// to avoid conflicts with custom router roots.
-config.resolver = {
-  ...config.resolver,
-  unstable_enablePackageExports: false,
-};
 
 module.exports = config;
